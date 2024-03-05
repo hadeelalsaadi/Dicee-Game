c27d79e8fc4e0a63e9d2d498ffab1be9c75b4e35
Hadeel AlSaadi <105803369+hadeelalsaadi@users.noreply.github.com>
2024-03-05 09:23:45 +0000

    index.js

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 var randomNamber =  Math.floor((Math.random() *6)+1);
 console.log(randomNamber);
 if (randomNamber==1){
-    document.querySelector("#p1").setAttribute("src","./images/dice1.png")
+    document.querySelector("#p1").setAttribute("src","dice1.png")
 }else if (randomNamber==2){
-    document.querySelector("#p1").setAttribute("src","./images/dice2.png")
+    document.querySelector("#p1").setAttribute("src","dice2.png")
 }else if (randomNamber=3){
-    document.querySelector("#p1").setAttribute("src","./images/dice3.png")
+    document.querySelector("#p1").setAttribute("src","dice3.png")
 }else if (randomNamber==4){
-    document.querySelector("#p1").setAttribute("src","./images/dice4.png")
+    document.querySelector("#p1").setAttribute("src","dice4.png")
 }else if (randomNamber==5){
-    document.querySelector("#p1").setAttribute("src","./images/dice5.png")
+    document.querySelector("#p1").setAttribute("src","dice5.png")
 }else{
-    document.querySelector("#p1").setAttribute("src","./images/dice6.png")
+    document.querySelector("#p1").setAttribute("src","dice6.png")
 }
 
 var randomNamber1 =  Math.floor((Math.random() *6)+1);
 console.log(randomNamber1);
 if (randomNamber1==1){
-    document.querySelector("#p2").setAttribute("src","./images/dice1.png")
+    document.querySelector("#p2").setAttribute("src","dice1.png")
 }else if (randomNamber1==2){
-    document.querySelector("#p2").setAttribute("src","./images/dice2.png")
+    document.querySelector("#p2").setAttribute("src","dice2.png")
 }else if (randomNamber1=3){
-    document.querySelector("#p2").setAttribute("src","./images/dice3.png")
+    document.querySelector("#p2").setAttribute("src","dice3.png")
 }else if (randomNamber1==4){
-    document.querySelector("#p2").setAttribute("src","./images/dice4.png")
+    document.querySelector("#p2").setAttribute("src","dice4.png")
 }else if (randomNamber1==5){
-    document.querySelector("#p2").setAttribute("src","./images/dice5.png")
+    document.querySelector("#p2").setAttribute("src","dice5.png")
 }else{
-    document.querySelector("#p2").setAttribute("src","./images/dice6.png")
+    document.querySelector("#p2").setAttribute("src","dice6.png")
 }
 if(randomNamber>randomNamber1){
     document.querySelector("h1").innerHTML= "The winner is Player 1";
